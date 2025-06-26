11c5083a63241b52ac83ae20feab85b1e18a0a96
KevinLIOUST <kevin.lioust@outlook.fr>
2025-06-26 11:40:54 +0200

    Création de la fonction pour initialiser la partie et pour utiliser une lettre ! :)

--- js/script.js
+++ js/script.js
@@ -5,9 +5,14 @@ let tabMots = [
     ["manga", "fairytail", "15"],
     ["jeu vidéo", "horizon zero dawn", "20"],
     ["film", "matrix", "10"],
-    ["serie", "the walking dead", "15"],
+    ["série", "the walking dead", "15"],
     ["youtuber", "squeezie", "10"]
 ]
+
+// Interupteurs :
+// 0 : Lettre pas encore utilisée.
+// 1 : Lettre utilisée.
+let tabInterupteursLettres = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 // Le nombre aléatoire tiré à chaque début de game
 let nbAleatoire = 0;
@@ -18,8 +23,8 @@ let categorie = "";
 // Récupérer le mot à chercher
 let motAchercher = "";
 
-// Mot a rechercher, mais avec des underscores.
-let motAchercherUnderscores = "";
+// // Mot a rechercher, mais avec des underscores.
+// let motAchercherUnderscores = "";
 
 // Récupérer le nombre d'essais pour trouver le mot
 let nbEssais = "";
@@ -27,33 +32,67 @@ let nbEssais = "";
 // Nombre d'underscores à afficher pour trouver le mot
 let nbUnderscores = 0;
 
-// Fonction pour lancer une partie
-function lancerPartie() {
+// Fonction pour utiliser une lettre
+function utiliserLettre(lettre) {
+    for (let i = 0; i < motAchercher.length; i++) {
+        if (motAchercher.charAt(i) == lettre) {
 
-    motAchercherUnderscores = "";
+        }
+    }
+    // for (let i = 0; i < motAchercher.length; i++) {
+    //     if (motAchercher.charAt(i) == lettre) {
+    //         document.getElementById("motAchercher").innerText = "";
+    //         for (let j = 0; j < motAchercher.length; j++) {
+    //             if (motAchercher.charAt(j) == lettre) {
+    //                 document.getElementById("motAchercher").innerText += lettre;
+    //             } else {
+    //                 if (motAchercher.charAt(j) == " ") {
+    //                     let motAreconstituer = document.getElementById("motAchercher").getHTML();
+    //                     document.getElementById("motAchercher").innerText = motAreconstituer + "\u00A0";
+    //                 }
+    //                 let motAreconstituer = document.getElementById("motAchercher").getHTML();
+    //                 document.getElementById("motAchercher").innerText = motAreconstituer + "_";
+    //             }
+    //         }
+    //     }
+    // }
+}
+
+// Fonction pour initialiser une partie
+function initialiserPartie() {
+
+    motAchercher = "";
 
     // Initialisation de la partie
-    nbAleatoire = Math.floor(Math.random() * 4);
+    nbAleatoire = Math.floor(Math.random() * 5);
+    // console.log(nbAleatoire);
+
     categorie = tabMots[nbAleatoire][0];
+    // console.log(categorie);
+
     motAchercher = tabMots[nbAleatoire][1];
+    // console.log(motAchercher);
+
     nbEssais = parseInt(tabMots[nbAleatoire][2]);
+    // console.log(nbEssais);
 
     // Affichage du nombre d'essais
     document.getElementById("nbEssais").innerText = nbEssais;
 
+    // Affichage de la catégorie du mot à trouver
+    document.getElementById("categorie").innerText = categorie;
+
     // Pour afficher le nombre d'underscores pour trouver le mot
     nbUnderscores = motAchercher.length;
 
-    for (let i = 1; i <= nbUnderscores; i++) {
-        motAchercherUnderscores += "_";
-    }
+    // Pour remettre à 0 les underscores du mot à trouver
+    document.getElementById("motAchercher").innerText = "";
 
-    // Remplacer les espaces par des tirets
-    for (let i = 0; i < motAchercher.length; i++) {
+    for (let i = 0; i < nbUnderscores; i++) {
         if (motAchercher.charAt(i) == " ") {
-            motAchercherUnderscores.charAt(i) += "-";
+            document.getElementById("motAchercher").innerText += "\u00A0";
+        } else {
+            document.getElementById("motAchercher").innerText += "_";
         }
     }
-
-    document.getElementById("motAchercher").innerText = motAchercherUnderscores;
 }
