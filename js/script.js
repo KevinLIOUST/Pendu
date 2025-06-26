f5d81af13ef1881bb2102761fdc075533c9a6a8f
KevinLIOUST <kevin.lioust@outlook.fr>
2025-06-26 17:55:16 +0200

    Quasi-finition du pendu ! :)

--- js/script.js
+++ js/script.js
@@ -1,18 +1,43 @@
 // Ingrédients
 
+// Tableau de toutes les lettres de l'alphabet
+let tabLettres = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
 // Les mots disponibles pour jouer au jeu
 let tabMots = [
-    ["manga", "fairytail", "15"],
-    ["jeu vidéo", "horizon zero dawn", "20"],
-    ["film", "matrix", "10"],
-    ["série", "the walking dead", "15"],
-    ["youtuber", "squeezie", "10"]
-]
-
-// Interupteurs :
-// 0 : Lettre pas encore utilisée.
-// 1 : Lettre utilisée.
-let tabInterupteursLettres = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  ["manga", "fairytail", "15"],
+  ["jeu vidéo", "horizon zero dawn", "20"],
+  ["film", "matrix", "10"],
+  ["série", "the walking dead", "15"],
+  ["youtuber", "squeezie", "10"],
+];
 
 // Le nombre aléatoire tiré à chaque début de game
 let nbAleatoire = 0;
@@ -23,8 +48,8 @@ let categorie = "";
 // Récupérer le mot à chercher
 let motAchercher = "";
 
-// // Mot a rechercher, mais avec des underscores.
-// let motAchercherUnderscores = "";
+// Mot à reconstituer pour gagner la partie
+let motArefaire = "";
 
 // Récupérer le nombre d'essais pour trouver le mot
 let nbEssais = "";
@@ -32,67 +57,102 @@ let nbEssais = "";
 // Nombre d'underscores à afficher pour trouver le mot
 let nbUnderscores = 0;
 
+// Le nombre de lettres manquantes pour trouver le mot et gagner la partie
+let nbLettresManquantes = 0;
+
 // Fonction pour utiliser une lettre
 function utiliserLettre(lettre) {
-    for (let i = 0; i < motAchercher.length; i++) {
-        if (motAchercher.charAt(i) == lettre) {
-
-        }
+  for (let i = 0; i < motAchercher.length; i++) {
+    if (motAchercher[i] === lettre) {
+        console.log(typeof motArefaire[i]);
+      motArefaire[i] = lettre;
+      console.log(lettre);
+      console.log(motAchercher[i]);
+      console.log(motArefaire[i]);
+    } else {
+      motArefaire[i] = "z";
     }
-    // for (let i = 0; i < motAchercher.length; i++) {
-    //     if (motAchercher.charAt(i) == lettre) {
-    //         document.getElementById("motAchercher").innerText = "";
-    //         for (let j = 0; j < motAchercher.length; j++) {
-    //             if (motAchercher.charAt(j) == lettre) {
-    //                 document.getElementById("motAchercher").innerText += lettre;
-    //             } else {
-    //                 if (motAchercher.charAt(j) == " ") {
-    //                     let motAreconstituer = document.getElementById("motAchercher").getHTML();
-    //                     document.getElementById("motAchercher").innerText = motAreconstituer + "\u00A0";
-    //                 }
-    //                 let motAreconstituer = document.getElementById("motAchercher").getHTML();
-    //                 document.getElementById("motAchercher").innerText = motAreconstituer + "_";
-    //             }
-    //         }
-    //     }
-    // }
+  }
+
+  document.getElementById("motAchercher").innerText = motArefaire;
+  console.log(motAchercher);
+  console.log(motArefaire);
+
+  if (nbEssais == 0 && nbLettresManquantes > 0) {
+    document.getElementById("partiePerdueOuGagnee").innerText =
+      "Vous avez perdue la partie ! ):";
+  } else if (motAchercher == motArefaire) {
+    document.getElementById("partiePerdueOuGagnee").innerText =
+      "Vous avez gagné(e) la partie ! :)";
+  } else {
+    document.getElementById("partiePerdueOuGagnee").innerText =
+      "Partie en cours";
+  }
+  let boutonAdesactiver = document.getElementById("btn" + lettre.toUpperCase());
+  boutonAdesactiver.disabled = true;
+  nbLettresManquantes--;
+  nbEssais--;
+  document.getElementById("nbEssais").innerText = nbEssais;
 }
 
 // Fonction pour initialiser une partie
 function initialiserPartie() {
+  motAchercher = "";
+  motArefaire = "";
 
-    motAchercher = "";
+  // Initialisation de la partie
+  nbAleatoire = Math.floor(Math.random() * 5);
+  // console.log(nbAleatoire);
 
-    // Initialisation de la partie
-    nbAleatoire = Math.floor(Math.random() * 5);
-    // console.log(nbAleatoire);
+  categorie = tabMots[nbAleatoire][0];
+  // console.log(categorie);
 
-    categorie = tabMots[nbAleatoire][0];
-    // console.log(categorie);
+  motAchercher = tabMots[nbAleatoire][1];
+  // console.log(motAchercher);
 
-    motAchercher = tabMots[nbAleatoire][1];
-    // console.log(motAchercher);
-
-    nbEssais = parseInt(tabMots[nbAleatoire][2]);
-    // console.log(nbEssais);
-
-    // Affichage du nombre d'essais
-    document.getElementById("nbEssais").innerText = nbEssais;
-
-    // Affichage de la catégorie du mot à trouver
-    document.getElementById("categorie").innerText = categorie;
-
-    // Pour afficher le nombre d'underscores pour trouver le mot
-    nbUnderscores = motAchercher.length;
-
-    // Pour remettre à 0 les underscores du mot à trouver
-    document.getElementById("motAchercher").innerText = "";
-
-    for (let i = 0; i < nbUnderscores; i++) {
-        if (motAchercher.charAt(i) == " ") {
-            document.getElementById("motAchercher").innerText += "\u00A0";
-        } else {
-            document.getElementById("motAchercher").innerText += "_";
-        }
+  for (let i = 0; i < motAchercher.length; i++) {
+    if (motAchercher.charAt(i) == " ") {
+      motArefaire += "\u00A0";
+    } else {
+      motArefaire += "_";
     }
+  }
+  console.log(motArefaire);
+
+  nbEssais = parseInt(tabMots[nbAleatoire][2]);
+  // console.log(nbEssais);
+
+  // Affichage du nombre d'essais
+  document.getElementById("nbEssais").innerText = nbEssais;
+
+  // Affichage de la catégorie du mot à trouver
+  document.getElementById("categorie").innerText = categorie;
+
+  // Pour afficher le nombre d'underscores pour trouver le mot
+  nbUnderscores = motAchercher.length;
+
+  // Pour récupérer le nombre de lettres manquantes pour trouver le mot
+  nbLettresManquantes = motAchercher.length;
+
+  // Pour remettre à 0 les underscores du mot à trouver
+  document.getElementById("motAchercher").innerText = "";
+
+  //   for (let i = 0; i < nbUnderscores; i++) {
+  //     if (motAchercher.charAt(i) == " ") {
+  //       document.getElementById("motAchercher").innerText += "\u00A0";
+  //     } else {
+  //       document.getElementById("motAchercher").innerText += "_";
+  //     }
+  //   }
+  document.getElementById("motAchercher").innerText = motArefaire;
+
+  document.getElementById("partiePerdueOuGagnee").innerText =
+    "Partie en cours...";
+
+  for (let i = 0; i < tabLettres.length; i++) {
+    let boutonAdesactiver = document.getElementById(
+      "btn" + tabLettres[i].toUpperCase()
+    );
+    boutonAdesactiver.disabled = false;
+  }
 }
